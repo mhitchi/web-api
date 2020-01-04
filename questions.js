bdca334e5feadad2f4f1439a1d26e65add88d9b8
mhitchi <mayhitchings@gmail.com>
2020-01-04 15:28:29 -0500

    added mock questions

--- questions.js
+++ questions.js
@@ -1,28 +1,30 @@
-var questions = [
+const myQuestions = [
   {
-    title: "",
-    choices: [],
-    answer: ""
+    question: "Who is the strongest?",
+    answers: {
+      a: "Superman",
+      b: "The Terminator",
+      c: "Waluigi, obviously"
+    },
+    correctAnswer: "c"
   },
   {
-    title: "",
-    choices: [],
-    answer: ""
+    question: "What is the best site ever created?",
+    answers: {
+      a: "SitePoint",
+      b: "Simple Steps Code",
+      c: "Trick question; they're both the best"
+    },
+    correctAnswer: "c"
   },
   {
-    title: "",
-    choices: [],
-    answer: ""
-  },
-  {
-    title: "",
-    choices: [],
-    answer: ""
-  },
-  {
-    title: "",
-    choices: [],
-    answer: ""
-  },
-  
-]
+    question: "Where is Waldo really?",
+    answers: {
+      a: "Antarctica",
+      b: "Exploring the Pacific Ocean",
+      c: "Sitting in a tree",
+      d: "Minding his own business, so stop asking"
+    },
+    correctAnswer: "d"
+  }
+];
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// h1, p, ul, h2, button
+// h1, p, div, h2, button
 var header = document.querySelector("h1");
 var paragraph = document.querySelector("p");
-var list = document.querySelector("ul");
+var quiz = document.getElementById("quiz");
 // header2= right/wrong
 var header2 = document.querySelector("h2");
 var submitBtn = document.querySelector("button");
@@ -13,9 +13,23 @@ var time = document.getElementById("time");
 // view highscores
 // time = 0
 // start quiz btn
+  submitBtn.addEventListener("click", buildQuiz);
   // time set to 75 sec, starts countdown
   // triggers question 1, list of answers, submit btn
     // submit btn triggers question 2 and so on
+
+    function buildQuiz() {
+
+    }
+
+
+    function correct() {
+
+    }
+
+    function incorrect() {
+
+    }
 
     // correct answer scores 10 points
     // incorrect answer scores -5 points
