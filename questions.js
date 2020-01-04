99ebee4f34f56874b904d5d0a65fe1374407331d
mhitchi <mayhitchings@gmail.com>
2020-01-04 15:59:10 -0500

    trying out a styling from tutorial

--- questions.js
+++ questions.js
@@ -28,3 +28,5 @@ const myQuestions = [
     correctAnswer: "d"
   }
 ];
+
+export {myQuestions};
--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-// h1, p, div, h2, button
-var header = document.querySelector("h1");
-var paragraph = document.querySelector("p");
-var quiz = document.getElementById("quiz");
-// header2= right/wrong
-var header2 = document.querySelector("h2");
-var submitBtn = document.querySelector("button");
-// highscore
-var highscore = document.getElementById("highscore");
-// time
-var time = document.getElementById("time");
+// // h1, p, div, h2, button
+// var header = document.querySelector("h1");
+// var paragraph = document.querySelector("p");
+// var quiz = document.getElementById("quiz");
+// // header2= right/wrong
+// var header2 = document.querySelector("h2");
+// var submitBtn = document.querySelector("button");
+// // highscore
+// var highscore = document.getElementById("highscore");
+// // time
+// var time = document.getElementById("time");
 
-// view highscores
-// time = 0
-// start quiz btn
-  submitBtn.addEventListener("click", buildQuiz);
-  // time set to 75 sec, starts countdown
-  // triggers question 1, list of answers, submit btn
-    // submit btn triggers question 2 and so on
+// // get questions
+// import{myQuestions} from './questions.js'
 
-    function buildQuiz() {
+// // view highscores
+// // time = 0
+// // start quiz btn
+//   submitBtn.addEventListener("click", buildQuiz);
+//   // time set to 75 sec, starts countdown
+//   // triggers question 1, list of answers, submit btn
+//     // submit btn triggers question 2 and so on
 
-    }
+//     function buildQuiz() {
+      
+//     }
+
+//     function nextQuestion() {
+
+//     }
+
+//     function selectAnswer() {
+
+//     }
 
 
-    function correct() {
+//     function correct() {
 
-    }
+//     }
 
-    function incorrect() {
+//     function incorrect() {
 
-    }
+//     }
 
-    // correct answer scores 10 points
-    // incorrect answer scores -5 points
-    // final score + seconds left
+//     // correct answer scores 10 points
+//     // incorrect answer scores -5 points
+//     // final score + seconds left
 
-    // highscore addEventListener shows highscore from localStorage
+//     // highscore addEventListener shows highscore from localStorage
